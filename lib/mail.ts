93d027d3f347fd29955f1d15deaa4757e0171751
Rahul Kumar Maity <103112746+foxyfool@users.noreply.github.com>
2024-07-09 19:31:53 +0530

    mailT

--- lib/mail.ts
+++ lib/mail.ts
@@ -6,7 +6,7 @@ const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@areyouwinningson.com",
     to: email,
     subject: "Two-Factor Code",
     html: `<div style="text-align: center; background-color: #F0F8FF; padding: 20px; border-radius: 5px;"> <p style="color: #007BFF; font-size: 18px;">Your Two-Factor Authentication code on 🔐Identity is:  ${token}</p></div>`,
@@ -17,7 +17,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@areyouwinningson.com",
     to: email,
     subject: "Reset your Password",
     html: `<div style="text-align: center; background-color: #F0F8FF; padding: 20px; border-radius: 5px;"> <p style="color: #007BFF; font-size: 18px;">Forgot your password on 🔐Identity? No worries , Simply reset your password  <a href="${resetLink}"> Here! </a></p></div>`,
@@ -28,7 +28,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@areyouwinningson.com",
     to: email,
     subject: "Confrim your email",
     html: `<div style="text-align: center; background-color: #F0F8FF; padding: 20px; border-radius: 5px;"> <p style="color: #007BFF; font-size: 18px;">Thanks for signing up on  🔐Identity! Please verify your email  <a href="${confirmLink}"> Here! </a></p></div>`,
